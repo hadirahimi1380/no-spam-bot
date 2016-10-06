3a7263dde9257ea31870a0a3a156291a556fd13d
hadirahimi1380 <hadirahimi1380@yahoo.com>
2016-10-06 20:29:02 +0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {253384387,77174278,253384387,0,tonumber(our_id)},--Sudo users
+    sudo_users = {253384387,290850946,290850946,77174278,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
